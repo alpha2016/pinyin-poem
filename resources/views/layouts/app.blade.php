ac8aeb192f3ece84b00e27e3307b154d904ebd83
hexiaodong <river0314@163.com>
2018-01-17 15:24:01 +0800

    npm 修正 poem页面完全重写，更加有兼容性，更加清晰干净

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="description" content="@yield('keywords')">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @stack('style')
 </head>
 <body>
     <div id="app">
@@ -29,22 +32,22 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', '歌谣') }}
                     </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
+                    {{--  <ul class="nav navbar-nav">
                         &nbsp;
-                    </ul>
+                    </ul>  --}}
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('login') }}">登录</a></li>
+                            <li><a href="{{ route('register') }}">注册</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -56,7 +59,7 @@
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            退出
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -76,5 +79,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    @stack('script')
 </body>
 </html>
--- resources/views/poem.blade.php
+++ resources/views/poem.blade.php
@@ -1,58 +1,61 @@
 @extends('layouts.app')
 
+@section('title', $poem->title . "&nbsp;|&nbsp;" . $poem->author)
+
+@section('keywords', $poem->title . ',' . $poem->author . ',' . $poem->type->name)
+
+@section('description', $poem->title . ',' . $poem->author . ',' . $poem->type->name)
+
 @push('style')
+
 <style>
-    rt {
-        font-size: 18px;
-        padding-left: 10px;
-    }
-    .row {
-        background : #ffffff;
-    }
+	.container { 
+		color: #2a2525;  font-family: "Microsoft YaHei","Segoe UI", "Lucida Grande", Helvetica, Arial,sans-serif; 
+	}
+    ruby { font-weight: 500; font-size: 1.05em;}
+	rt { font-weight: 500; font-size: 0.59em; }
 </style>
 @endpush
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-11 col-md-offset-1">
-			<!-- Section Image -->
-			<div class="row no-margin padding-onlytop-lg">
-				<div class="col-md-6 padding-leftright-null">
-					<div data-responsive="parent-height" data-responsive-id="about" class="text">
-						<h3 class="margin-bottom-null left">{{ $poem->title }}
-                                  <small>{{ $poem->author }}</small>
-                        </h3>
-						<div class="padding-onlytop-sm">
-							@foreach($results as $result)
-								<p class='margin-bottom grey'>
-									@foreach($result as $key => $val)
-										<ruby>
-											{{ $val[0] }}
-											<rt>{{ $val[1] }}</rt>
-										</ruby>
-									@endforeach
-								</p>
-							@endforeach
-
-							{{--<a href="#" class="btn-pro">Read More</a>--}}
-                            <blockquote>
-                                <p>
-                                    <small>{{ $poem->description }}</small>
-                                </p>
-                            </blockquote>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 padding-leftright-null">
-					<div data-responsive="child-height" data-responsive-id="about" class="section-image height-auto-sm">
-						<picture class="section right">
-							<img src="{{ $poem->picture }}"  class="img-responsive" alt="{{ $poem->title }}">
-						</picture>
-					</div>
+	<div class="container">
+		{{--  <div><a href="/">首页</a> >> {{ $poem->type->name }}</div>  --}}
+		<div class="row">
+				<div class="span6">
+					<ul class="breadcrumb">
+						<li>
+							<a href="/">首页</a> <span class="divider">>></span>
+						</li>
+						{{ $poem->type->name }}
+					</ul>
 				</div>
 			</div>
-		</div>
+		<h3>{{ $poem->title }}
+			<small>{{ $poem->author }}</small>
+		</h3>
+		<p>
+			<img src="{{ $poem->picture }}" alt="{{ $poem->title }}" class='img-responsive center-block' src="http://d.hiphotos.baidu.com/image/pic/item/a044ad345982b2b713b5ad7d3aadcbef76099b65.jpg"
+			/>
+		</p>
+
+		<hr>
+			@foreach($results as $result)
+				<p class='margin-bottom grey'>
+					@foreach($result as $key => $val)
+						<ruby>
+							{{ $val[0] }}
+							<rt>{{ $val[1] }}</rt>
+						</ruby>
+					@endforeach
+				</p>
+			@endforeach
+			
+		<hr>
+			<blockquote>
+				<small>{{ $poem->description }}</small>
+			</blockquote>
+			
+		<hr>
+		<h5>更新于：{{ $poem->created_at }}</h5>
 	</div>
-</div>
 @endsection
