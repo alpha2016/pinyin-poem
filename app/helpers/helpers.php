dd2b7d70229b404b7799e6706ae253849d83223c
hexiaodong <river0314@163.com>
2018-03-31 15:36:52 +0800

    一个批量更新的助手方法

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -128,7 +128,6 @@ if (!function_exists('sensitiveWordFilter')) {
 
         // 提取中文部分，防止其中夹杂英语等
         preg_match_all("/[\x{4e00}-\x{9fa5}]+/u", $str, $match);
-        $chinsesArray = $match[0];
         $chineseStr = implode('', $match[0]);
         $englishStr = strtolower(preg_replace("/[^A-Za-z0-9\.\-]/", " ", $str));
 
@@ -147,5 +146,49 @@ if (!function_exists('sensitiveWordFilter')) {
             }
         }
         return $flag ? 'yes' : 'no';
+    }
+}
+
+/**
+ * 批量更新的助手方法
+ */
+if (!function_exists('updateBatch')) {
+    function updateBatch($table, $multipleData = [])
+    {
+        try {
+            if (empty($multipleData)) {
+                throw new \Exception("数据不能为空");
+            }
+            $tableName = DB::getTablePrefix() . $table; // 表名
+            $firstRow = current($multipleData);
+
+            $updateColumn = array_keys($firstRow);
+            // 默认以id为条件更新，如果没有ID则以第一个字段为条件
+            $referenceColumn = isset($firstRow['id']) ? 'id' : current($updateColumn);
+            unset($updateColumn[0]);
+            // 拼接sql语句
+            $updateSql = "UPDATE " . $tableName . " SET ";
+            $sets = [];
+            $bindings = [];
+            foreach ($updateColumn as $uColumn) {
+                $setSql = "`" . $uColumn . "` = CASE ";
+                foreach ($multipleData as $data) {
+                    $setSql .= "WHEN `" . $referenceColumn . "` = ? THEN ? ";
+                    $bindings[] = $data[$referenceColumn];
+                    $bindings[] = $data[$uColumn];
+                }
+                $setSql .= "ELSE `" . $uColumn . "` END ";
+                $sets[] = $setSql;
+            }
+            $updateSql .= implode(', ', $sets);
+            $whereIn = collect($multipleData)->pluck($referenceColumn)->values()->all();
+            $bindings = array_merge($bindings, $whereIn);
+            $whereIn = rtrim(str_repeat('?,', count($whereIn)), ',');
+            $updateSql = rtrim($updateSql, ", ") . " WHERE `" . $referenceColumn . "` IN (" . $whereIn . ")";
+            // 传入预处理sql语句和对应绑定数据
+            return DB::update($updateSql, $bindings);
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 }
